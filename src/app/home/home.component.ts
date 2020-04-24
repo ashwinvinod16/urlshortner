d66fc0ecd45f77d83b07e239a9f8f332b6163f82
Ashwin Vinod <ashwivinod16@gmail.com>
2020-04-24 21:02:55 +0530

    changes applied

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit {
   id
   constructor(private servceobj:UrlserviceService,private activateroute: ActivatedRoute) { 
     this.id=this.activateroute.snapshot.params.id
+    console.log(this.id);
     if(this.id!=undefined){
     this.servceobj.redirecturl(this.id).subscribe((data)=>{
       window.location.href=data.urlName;
